838acd617a106e499c262e3389fcfdfa29cc12fd
gmh218 <gmh218@lehigh.edu>
2015-09-01 18:07:25 +0000

    Completed Homework 01 assignment

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////
+//Brian Chen
+//homework 01
+//Welcome Class java program
+//
+//first compile program
+//javac WelcomeClass.java
+//run program
+//java WelcomeClass//
+
+//define a class//
+public class WelcomeClass {
+    
+    //add main method
+public static void main(String[] args) {
+    //print the statement with box around it
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println(" ^  ^  ^  ^  ^  ^ ");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("  <-g-m-h-2-1-8-> ");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println(" v  v  v  v  v  v");
+    System.out.println("This summer I worked for a camp,");
+    System.out.println("and I spent a of time outdoors.");
+}
+}
